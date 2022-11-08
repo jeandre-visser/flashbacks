826ba180f7566663dbdec9d39fe391b27316be33
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-07 18:16:38 -0700

    Use redux and import our reducers to create store variable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { applyMiddleware, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(reducers, compose(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
