9ed18e741da84729773785e223c767e6b59c665f
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-07 19:11:11 -0700

    Update imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { legacy_createStore as createStore } from 'redux'
 import { applyMiddleware, compose } from 'redux';
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
+import { reducers }from './reducers';
 
-const store = configureStore(reducers, compose(applyMiddleware(thunk)))
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
