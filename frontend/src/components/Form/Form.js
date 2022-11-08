23228a4165ffc66741ab40a053ea0ff5b4570c69
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-08 15:23:29 -0700

    Build rest of form

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import useStyles from './styles';
 import { Typography, Button, TextField } from '@material-ui/core';
+import { MdPhotoCamera } from 'react-icons/md';
 
 const Form = () => {
   const classes = useStyles();
   const handleSubmit = () => {
+
+  }
+
+  const clear = () => {
 
   }
 
@@ -14,18 +19,59 @@ const Form = () => {
   
   return (
     <div className={classes.div}>
-      <form noValidate autoComplete='off' onSubmit={handleSubmit} className={classes.form}>
+      <form noValidate autoComplete='off' onSubmit={handleSubmit} className={`${classes.root} ${classes.form}`}>
         <Typography variant="h6" className={classes.p}>
           Flashback
         </Typography>
         <TextField 
-          name="user" 
+          name="User" 
+          variant="outlined" 
+          label="User" 
+          fullWidth
+          value={postData.user}
+          onChange={(event) => setPostData({ ...postData, user: event.target.value })}
+        />
+        <TextField 
+          name="title" 
+          variant="outlined" 
+          label="Title" 
+          fullWidth
+          value={postData.title}
+          onChange={(event) => setPostData({ ...postData, title: event.target.value })}
+        />
+        <TextField 
+          name="message" 
           variant="outlined" 
           label="Your Flashback" 
           fullWidth
-          value={postData.user}
-          onChange={} 
+          value={postData.message}
+          onChange={(event) => setPostData({ ...postData, message: event.target.value })}
         />
+        <TextField 
+          name="tags" 
+          variant="outlined" 
+          label="tags" 
+          fullWidth
+          value={postData.tags}
+          onChange={(event) => setPostData({ ...postData, tags: event.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <Button variant="contained" component="label" disableElevation className={classes.photo}>
+            Upload <MdPhotoCamera size={20} style={{marginLeft: '5px'}} />
+          <input hidden accept="image/*" 
+            multiple={false}
+            type="file"
+            onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} />
+          </Button>
+        </div>
+        <Button 
+          className={classes.btn} 
+          fullWidth
+          variant="contained"
+          size="large"
+          type="submit"
+        >Post
+        </Button>
       </form>
     </div>
   )
