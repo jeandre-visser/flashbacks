70e2ffb8884c4d3c2c235487f1ff197c6a7153e7
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-08 15:44:35 -0700

    Post the data to the url as a new post

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // this url returns all the posts in database
 const url = 'http://localhost:8080/posts';
 
-// fetch the posts usingg axios
+// fetch the posts using axios
 export const fetchPosts = () => axios.get(url);
 
+export const createPost = (newPost) => axios.post(url, newPost);
