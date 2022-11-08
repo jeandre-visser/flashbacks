eab36dc62b4e82641bf8f55bebb6929215538a3d
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-07 17:40:38 -0700

    Use makeStyles from mui to style components

---
+++ frontend/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 20,
+    margin: '15px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '15px',
+    backgroundColor: '#057be9',
+  },
+  image: {
+    marginLeft: '15px',
+    width: '200px'
+  },
+}));
