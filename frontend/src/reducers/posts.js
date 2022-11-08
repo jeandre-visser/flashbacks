f19cedfb14bd9938606b5c4cd8a1f9ace9d1752b
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-07 18:14:26 -0700

    Create the posts reducer using redux

---
+++ frontend/src/reducers/posts.js
@@ -0,0 +1,12 @@
+// Create reducer with a state of 'posts'
+export default (posts = [], action) => {
+  // switch statement returns based on action
+  switch(action.type) {
+    case 'FETCH_ALL':
+      return posts;
+    case "CREATE":
+      return posts;
+    default: 
+      return posts;
+  }
+}
