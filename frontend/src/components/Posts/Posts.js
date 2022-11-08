2bc7e19b77b7ed3046221caca6e4303654654c7a
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-07 17:00:42 -0700

    Create folders and files for the Form, Posts, and Post

---
+++ frontend/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './post';
+
+const Posts = () => {
+  return (
+    <>
+      <div>Posts</div>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default Posts
