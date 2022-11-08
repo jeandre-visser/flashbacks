b9ca7fe2fade6b6ead35f9a2340eb68f40f63900
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-07 19:35:24 -0700

    Start building form for a user to make a post

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import useStyles from './styles';
+import { Typography, Button, TextField } from '@material-ui/core';
 
 const Form = () => {
   const classes = useStyles();
+  const handleSubmit = () => {
+
+  }
   
   return (
-    <div>Form</div>
+    <div className={classes.div}>
+      <form noValidate autoComplete='off' onSubmit={handleSubmit} className={classes.form}>
+        <Typography variant="h6">
+          Flashback
+        </Typography>
+        <TextField 
+          name="user" 
+          variant="outlined" 
+          label="Your Flashback" 
+          fullWidth 
+        />
+      </form>
+    </div>
   )
 }
 
