7d90f946db141b9b4a189b757b8be472517c9079
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-08 14:09:06 -0700

    Add text field and create a state for the post data

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './styles';
 import { Typography, Button, TextField } from '@material-ui/core';
 
@@ -7,18 +7,24 @@ const Form = () => {
   const handleSubmit = () => {
 
   }
+
+  const [ postData, setPostData ] = useState({
+    user: '', title: '', message: '', tags: '', selectedFile: ''
+  });
   
   return (
     <div className={classes.div}>
       <form noValidate autoComplete='off' onSubmit={handleSubmit} className={classes.form}>
-        <Typography variant="h6">
+        <Typography variant="h6" className={classes.p}>
           Flashback
         </Typography>
         <TextField 
           name="user" 
           variant="outlined" 
           label="Your Flashback" 
-          fullWidth 
+          fullWidth
+          value={postData.user}
+          onChange={} 
         />
       </form>
     </div>
