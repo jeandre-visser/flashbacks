8b8f5bd377910b7e0d60ead2a449c4ffc72cf5c4
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-08 15:23:39 -0700

    Style the form

--- frontend/src/components/Form/styles.js
+++ frontend/src/components/Form/styles.js
@@ -1,24 +1,47 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, StylesProvider } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
-      margin: theme.spacing(1),
+      margin: theme.spacing(0.5),
     },
   },
-  paper: {
+  div: {
     padding: theme.spacing(2),
+    boxShadow: '0px 0px 15px rgba(5, 123, 233, 1.0)',
+    borderRadius: '1.5rem',
+  },
+  p: {
+    marginBottom: '0.5rem',
   },
   form: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'center',
+    gap: '0.5rem'
   },
   fileInput: {
-    width: '92%',
+    width: '100%',
     margin: '12px 0',
   },
-  buttonSubmit: {
-    marginBottom: 8,
+  btn: {
+    marginBottom: "0.5rem",
+    backgroundColor: 'var(--secondary-color)',
+    color: "#FFF",
+    fontWeight: '600',
+    '&:hover': {
+      backgroundColor: '#FFF',
+      color: 'var(--secondary-color)'
+    }
   },
+  photo: {
+    backgroundColor: "var(--yellow-color)",
+    '&:hover': {
+      backgroundColor: '#FFF',
+      border: '2px solid var(--yellow-color)',
+      padding: '4px 14px',
+      color: 'var(--yellow-color)',
+      transition: '0s all ease-in-out'
+    }
+  }
 }));
