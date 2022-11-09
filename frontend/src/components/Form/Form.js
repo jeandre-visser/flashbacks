28afad9a30f023c23fd08bccd4cca7cc2d74b5de
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-08 18:25:27 -0700

    Erase all fields when pressing on post

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -10,9 +10,12 @@ import { useSelector } from 'react-redux';
 const Form = ({ currentId, setCurrentId }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [postData, setPostData] = useState({
+    user: '', title: '', message: '', tags: '', selectedFile: ''
+  });
 
   const post = useSelector((state) => currentId ? state.posts.find((pos) => pos._id === currentId) : '');
-  
+
   useEffect(() => {
     if (post) setPostData(post);
   }, [post]);
@@ -25,18 +28,22 @@ const Form = ({ currentId, setCurrentId }) => {
     } else {
       dispatch(createPost(postData));
     }
+    erase();
   }
 
-  const [postData, setPostData] = useState({
-    user: '', title: '', message: '', tags: '', selectedFile: ''
-  });
+  const erase = () => {
+    setCurrentId(null);
+    setPostData({
+      user: '', title: '', message: '', tags: '', selectedFile: ''
+    });
+  }
   
   
   return (
     <div className={classes.div}>
       <form noValidate autoComplete='off' onSubmit={handleSubmit} className={`${classes.root} ${classes.form}`}>
         <Typography variant="h6" className={classes.p}>
-          Flashback
+          {currentId ? 'Edit' : 'Post'} Your Flashback
         </Typography>
         <TextField 
           name="user" 
