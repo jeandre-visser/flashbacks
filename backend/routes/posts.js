c6a5bad559bdb36f8fb0d7d6bd456eb0da552ca6
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-09 15:29:49 -0700

    Import deletePost

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js';
 
 router.get('/', getPosts);
 router.post('/', createPost);
