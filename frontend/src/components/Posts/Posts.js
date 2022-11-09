5d06a9e50ac8c6d6ffa34c2cc2bbaa7d4b7bf118
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-08 17:33:23 -0700

    Display Post card, otherwise show a loading circle

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import Post from './Post/Post';
 import useStyles from './styles';
+import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 
 const Posts = () => {
   const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
   
   return (
-    <>
-      <div>Posts</div>
-      <Post />
-      <Post />
-      <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid
+        container
+        alignItems="stretch"
+        spacing={2}
+        className={classes.container}
+      >
+        {posts.map((post) => (
+          <Grid item key={post._id} xs={12} sm={4} >
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   )
 }
 
