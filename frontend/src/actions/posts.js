445ec7474cfb05cf0faf297594ec58ab6d13f211
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-08 18:35:44 -0700

    Fix spelling error

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -33,6 +33,6 @@ export const updatePost = (id, post) => async (dispatch) => {
     dispatch({ type: 'UPDATE', payload: data });
 
   } catch (error) {
-    console.logg(error);
+    console.log(error);
   }
 }
