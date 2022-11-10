93d6df5295aea6fcaa24f131bdfe7c426ae2ece7
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-10 14:14:46 -0700

    Implement routes to Home page and authentication page using react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
+import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 const App = () => {
 
   return (
-    <Container maxWidth="xl" >
-      <Navbar />
-      <Home />
-    </Container>
+    <BrowserRouter >
+      <Container maxWidth="xl" >
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   )
 }
 
