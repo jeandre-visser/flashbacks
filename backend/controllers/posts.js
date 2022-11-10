b15bcfdf2ebaa2dd63e4e731cc253e08f18f01f7
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-09 17:09:33 -0700

    Remove console.log

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -37,9 +37,7 @@ export const deletePost = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('That id does not exist.');
 
   await PostMessage.findByIdAndRemove(id);
-
-  console.log('DELETE')
-
+  
   res.json({ message: "Post successfully deleted"});
 };
 
