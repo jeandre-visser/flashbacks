f256ce3c0ef7da6729d0eb5b40092aba7d7266f1
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-10 14:45:32 -0700

    Initial build for authentication page

---
+++ frontend/src/components/Auth/Auth.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Avatar, Grid, Typography, Container, TextField, Button } from '@material-ui/core';
+import useStyles from './styles';
+import LockIcon from '@material-ui/icons/Lock';
+
+const Auth = () => {
+  
+  const classes = useStyles();
+
+  const isRegister = false;
+
+  const handleOnSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <div className={classes.page}>
+        <Avatar className={classes.avatar}>
+          <LockIcon />
+        </Avatar>
+        <Typography variant="h4">
+          {isRegister ? 'Register' : "Sign In"}
+        </Typography>
+        <form className={classes.form} onSubmit={handleOnSubmit} >
+          <Grid spacing={1} container>
+           {isRegister && (
+            <>
+              <TextField 
+                autoFocus 
+                name="firstName" 
+                label="First Name" 
+                handleChange={handleChange} xs={6}
+              />
+              
+            </>
+           )}
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  )
+}
+
+export default Auth
