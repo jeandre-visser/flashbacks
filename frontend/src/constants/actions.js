a20be1307d5592cc12badb4d9b3e0beffe004ac9
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-10 17:55:15 -0700

    Add teh authReducer

--- frontend/src/constants/actions.js
+++ frontend/src/constants/actions.js
@@ -3,3 +3,6 @@ export const UPDATE = 'UPDATE';
 export const LIKE = 'LIKE';
 export const CREATE = 'CREATE';
 export const FETCH_ALL = 'FETCH_ALL';
+
+export const AUTH = 'AUTH';
+export const SIGNOUT = 'SIGNOUT';
